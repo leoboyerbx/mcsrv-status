0b488c0503e1df4ffec02001885729977ed8a9da
leoboyerbx <contact@leoboyer.fr>
2021-06-30 01:26:13 +0200

    WIP attachment

--- discord/index.js
+++ discord/index.js
@@ -1,14 +1,25 @@
 const ServerStatus = require('../minecraft/ServerStatus')
 const { token, prefix, onlineChannelId } = require('./config')
-const Discord = require('discord.js');
-const bot = require('bot-commander');
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const bot = require('bot-commander')
+const mergeImg = require('merge-img')
+const { promisify } = require('util')
+const client = new Discord.Client()
 require('discord-buttons')(client)
 
 const serverStatus = ServerStatus.getInstance()
 let onlineChannel
+let imgIndex = 0
 
-const getStatusEmbed = () => {
+const writeImage = (img, path) => {
+  return new Promise(resolve => {
+    img.write(path, () => {
+      resolve()
+    })
+  })
+}
+
+const getStatusEmbed = async () => {
   if (!serverStatus.serverOnline) {
     return new Discord.MessageEmbed()
       .setColor('#d53c3c')
@@ -16,41 +27,54 @@ const getStatusEmbed = () => {
   } else {
     const { players } = serverStatus
     let description = '**Joueurs actuellement connectés**:'
+    let attachment
+    const images = ['./public/blank.png']
     if (players?.length) {
       players.forEach(player => {
         description += '\n - ' + player.name
+        images.push('https://crafatar.com/renders/body/' + player.id + '?scale=2')
       })
     } else {
       description += '\n\n_Aucun joueur connecté._'
     }
-    return new Discord.MessageEmbed()
+    const img = await mergeImg(images, { offset: 20 })
+    await writeImage(img, './public/online-'+ imgIndex +'.png')
+    attachment = new Discord.MessageAttachment('./public/online-'+ imgIndex +'.png')
+    const embed = new Discord.MessageEmbed()
       .setColor('#40cbbe')
       .setDescription(description)
+    if (attachment) {
+      embed.attachFiles(attachment)
+        .setImage('attachment://online-'+ imgIndex +'.png')
+    }
+    imgIndex++
+    return embed
   }
 }
 
 const activeMessages = []
 const addStatusMessage = async (channel) => {
-  const embedMessage = getStatusEmbed()
+  const embedMessage = await getStatusEmbed()
   const message = await channel.send('_Statut en temps réel_', embedMessage)
   activeMessages.push(message.id)
 }
 
 const updateStatusMessages = async () => {
   const toDelete = []
-  const embedMessage = getStatusEmbed()
+  const embedMessage = await getStatusEmbed()
   for (const messageId of activeMessages) {
     try {
       const message = await onlineChannel.messages.fetch(messageId)
+      message.suppressEmbeds()
       message.edit('_Statut en temps réel_', embedMessage)
     } catch (e) {
       toDelete.push(messageId)
     }
   }
   toDelete.forEach(v => {
-    const index = activeMessages.indexOf(v);
+    const index = activeMessages.indexOf(v)
     if (index > -1) {
-      activeMessages.splice(index, 1);
+      activeMessages.splice(index, 1)
     }
   })
 }
@@ -64,22 +88,24 @@ bot
       addStatusMessage(message.channel)
     }
   })
-bot.command('tmp').action(() => {updateStatusMessages()})
+bot.command('tmp').action(() => {
+  updateStatusMessages()
+})
 
 module.exports = async () => {
   client.on('ready', async () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`)
     onlineChannel = await client.channels.fetch(onlineChannelId)
 
     serverStatus.on('data', () => {
       updateStatusMessages()
     })
-  });
+  })
 
   client.on('message', message => {
     bot.parse(message.content, message)
-  });
+  })
 
-  await client.login(token);
+  await client.login(token)
 
 }
